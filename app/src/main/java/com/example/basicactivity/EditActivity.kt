e71b024d3b3f26c0440e34c0f07703f2d892ce1e
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-05-31 22:45:57 +0900

    Realm transaction処理

--- app/src/main/java/com/example/basicactivity/EditActivity.kt
+++ app/src/main/java/com/example/basicactivity/EditActivity.kt
@@ -2,25 +2,54 @@ package com.example.basicactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.widget.Toast
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_edit.*
+import java.util.*
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the PeripheralManager
- * For example, the snippet below will open a GPIO pin and set it to HIGH:
- *
- * val manager = PeripheralManager.getInstance()
- * val gpio = manager.openGpio("BCM6").apply {
- *     setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * }
- * gpio.value = true
- *
- * You can find additional examples on GitHub: https://github.com/androidthings
- */
 class EditActivity : AppCompatActivity() {
+
+    private val tag = "BooldPressure"
+    private lateinit var realm:Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        realm = Realm.getDefaultInstance()
+        saveBtn.setOnClickListener {
+            var max: Long = 0
+            var min: Long = 0
+            var pulse: Long = 0
+
+            realm.executeTransaction {
+                val maxId = realm.where<BloodPress>().max("id")
+                val nextId = (maxId?.toLong() ?: 0L) + 1L
+                val booldPress = realm.createObject<BloodPress>(nextId)
+
+                if (!maxEdit.text.isNullOrEmpty()){
+                    max = maxEdit.text.toString().toLong()
+                }
+                if (!minEdit.text.isNullOrEmpty()){
+                    min = minEdit.text.toString().toLong()
+                }
+                if (!pulseEdit.text.isNullOrEmpty()){
+                    pulse = pulseEdit.text.toString().toLong()
+                }
+                booldPress.dateTime = Date()
+                booldPress.max = max
+                booldPress.min = min
+                booldPress.pulse = pulse
+            }
+            Toast.makeText(applicationContext, "保存しました", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
