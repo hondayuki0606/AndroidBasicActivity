8d5434a588432d7bc614ec900ce75cda0d1d2ede
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-06-06 21:55:19 +0900

    add CustomRecyclerViewAdapter

--- app/src/main/java/com/example/basicactivity/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/basicactivity/CustomRecyclerViewAdapter.kt
@@ -3,5 +3,19 @@ package com.example.basicactivity
 class CustomRecyclerViewAdapter(realmResults: RealmResults<BloodPress>) :
 RecyclerView.Adapter<ViewHolder>() {
     private val rResults: RealmResults<BloodPress> = realmResults
-    
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder{
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.one_result, parent, false)
+        val viewholder = VireHolder(view)
+        return viewholder
+    }
+
+    override fun getItemCount() :Int {
+        return rResults.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, postion: Int) {
+
+    }
 }
